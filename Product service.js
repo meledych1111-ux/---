e24baa6aaf3623b570d70eae455421d7d00beedf
meledych1111-ux <meledych1111@gmail.com>
2025-08-26 08:18:14 +0300

    Create Product service.js

---
+++ Product service.js
@@ -0,0 +1,41 @@
+import { products } from '../data/products.js';
+
+export class ProductService {
+    constructor() {
+        this.products = products;
+    }
+
+    getAllProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        return this.products.find(product => product.id === id);
+    }
+
+    getProductsByCategory(category) {
+        if (category === 'all') return this.products;
+        return this.products.filter(product => product.category === category);
+    }
+
+    searchProducts(searchText) {
+        const searchLower = searchText.toLowerCase();
+        return this.products.filter(product =>
+            product.name.toLowerCase().includes(searchLower) ||
+            product.description.toLowerCase().includes(searchLower)
+        );
+    }
+
+    filterByPrice(products, priceRange) {
+        switch (priceRange) {
+            case '0-5000':
+                return products.filter(product => product.price <= 5000);
+            case '5000-10000':
+                return products.filter(product => product.price > 5000 && product.price <= 10000);
+            case '10000+':
+                return products.filter(product => product.price > 10000);
+            default:
+                return products;
+        }
+    }
+}
