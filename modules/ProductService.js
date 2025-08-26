5ebaa43a306e65df02651e81ac02fb133003b3ce
meledych1111-ux <meledych1111@gmail.com>
2025-08-26 14:15:35 +0300

    Update ProductService.js

--- modules/ProductService.js
+++ modules/ProductService.js
@@ -18,24 +18,24 @@ export class ProductService {
         return this.products.filter(product => product.category === category);
     }
 
-    searchProducts(searchText, productsArray = this.products) {
+    searchProducts(searchText) {
         const searchLower = searchText.toLowerCase();
-        return productsArray.filter(product =>
+        return this.products.filter(product =>
             product.name.toLowerCase().includes(searchLower) ||
             product.description.toLowerCase().includes(searchLower)
         );
     }
 
-    filterByPrice(productsArray, priceRange) {
+    filterByPrice(products, priceRange) {
         switch (priceRange) {
             case '0-5000':
-                return productsArray.filter(product => product.price <= 5000);
+                return products.filter(product => product.price <= 5000);
             case '5000-10000':
-                return productsArray.filter(product => product.price > 5000 && product.price <= 10000);
+                return products.filter(product => product.price > 5000 && product.price <= 10000);
             case '10000+':
-                return productsArray.filter(product => product.price > 10000);
+                return products.filter(product => product.price > 10000);
             default:
-                return productsArray;
+                return products;
         }
     }
 }
